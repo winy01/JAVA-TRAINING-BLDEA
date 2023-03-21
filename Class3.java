1e7316620bb63f13bf10caa6d7b16cd13fb2d96f
HP <HP@DESKTOP-B8ATM6B>
2023-03-21 08:34:13 -0700

    added code

---
+++ Class3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Class3 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		char a='H';
+		System.out.println("The ASCII value of " +a+ " is "+(int)a);
+		
+	}
+
+}
